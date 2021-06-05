d9ebe6e4f51ce8518a5c096543fc67af9f1adc93
JustAyaz <43115720+JustAyaz@users.noreply.github.com>
2021-06-05 17:18:42 +0300

    daddy is here v2

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rizr!gk=08#dtg#ul(8u-c$a6s1d3t#u&lsmb4)y45!gm@z#4a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
